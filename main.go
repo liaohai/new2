5af9c4dd65346b56df1a9785829f2ba3db150676
888 <111@111.com>
2019-11-07 15:16:03 +0800

    commit

--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 
 func main() {
 	add := calc.Add(1, 10)
-	fmt.Println("Add(a, b int)",add)
+	fmt.Println("Add(1+ 10 int)",add)
 	fmt.Println("++++")
 
 	res :=calc.Sub(30 ,20)
-	fmt.Println("++++",res)
-	
-	res :=calc.Multi(30 ,20)
-	fmt.Println("====",res)
+	fmt.Println("Sub(30 - 20)",res)
 
-	res :=calc.Div(30 ,20)
-	fmt.Println("====",res)
+	res =calc.Multi(30 ,20)
+	fmt.Println("Multi(30 * 20)",res)
+
+	res =calc.Div(30 ,20)
+	fmt.Println("Div(30/20)",res)
 
 }
