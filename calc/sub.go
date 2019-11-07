221212f57c450634db4d264fe029b61bc2d18961
888 <111@111.com>
2019-11-07 20:14:59 +0800

    1111111111111111111111

--- calc/sub.go
+++ calc/sub.go
@@ -8,9 +8,9 @@ func Sub(a,b int) int {
 
 	fmt.Println("sub")
 	fmt.Println("sub")
+	fmt.Println("11!!")
+	fmt.Println("11!!")
+	fmt.Println("11!!")
 	return a - b
 
-	fmt.Println("11!!")
-	fmt.Println("11!!")
-	fmt.Println("11!!")
 }
