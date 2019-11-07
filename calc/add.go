0737822cf7e30694b6eeec81a2e02fdcea8daba5
abc <123@123.com>
2019-11-07 09:13:56 +0800

    two commit

--- calc/add.go
+++ calc/add.go
@@ -1,5 +1,5 @@
 package calc
 
-func Add(a , b int) int {
-
+func Add(a, b int) int {
+	return a + b
 }
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go5/01区块链/test/get/calc"
+)
 
+func main() {
+	add := calc.Add(1, 10)
+	fmt.Println("Add(a, b int)",add)
 }
